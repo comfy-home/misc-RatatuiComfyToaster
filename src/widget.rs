2cdbf6261919763772123bb9f1205ac224d68dc3
Tom @ ComfyHome® <dev@comfyhome.io>
2026-04-08 21:45:02 +0100

    feat: Add background color support to Toast widget

--- src/widget.rs
+++ src/widget.rs
@@ -1,4 +1,5 @@
 use ratatui::{
+    style::Color,
     style::Style,
     symbols::{self},
     widgets::{Block, Borders, Padding, Paragraph, Widget, WidgetRef, Wrap},
@@ -11,14 +12,16 @@ use crate::engine::ToastType;
 pub struct Toast {
     pub message: String,
     pub type_: ToastType,
+    pub bg: Color,
 }
 
 impl Toast {
     /// Creates a new `Toast` widget with the given message and type.
-    pub fn new(message: &str, type_: ToastType) -> Self {
+    pub fn new(message: &str, type_: ToastType, bg: Color) -> Self {
         Self {
             message: message.to_string(),
             type_,
+            bg,
         }
     }
 }
@@ -27,13 +30,15 @@ impl WidgetRef for Toast {
     fn render_ref(&self, area: ratatui::layout::Rect, buf: &mut ratatui::buffer::Buffer) {
         const PADDING: u16 = 1;
         let paragraph = Paragraph::new(self.message.as_str())
+            .style(Style::default().fg(Color::White).bg(self.bg))
             .wrap(Wrap { trim: false })
             .block(
                 Block::default()
                     .borders(Borders::LEFT | Borders::RIGHT)
                     .border_set(symbols::border::QUADRANT_OUTSIDE)
                     .padding(Padding::uniform(PADDING))
-                    .border_style(Style::default().fg(self.type_.into())),
+                    .style(Style::default().bg(self.bg))
+                    .border_style(Style::default().fg(self.type_.into()).bg(self.bg)),
             );
         paragraph.render(area, buf);
     }
